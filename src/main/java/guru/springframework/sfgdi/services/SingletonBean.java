6311b3eb97c994b525cef8fdba4c7ce0491c424d
Michaël <mragaven@maureva.com>
2021-11-22 10:38:56 +0400

    scope example

---
+++ src/main/java/guru/springframework/sfgdi/services/SingletonBean.java
@@ -0,0 +1,19 @@
+package guru.springframework.sfgdi.services;
+
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Scope(ConfigurableBeanFactory.SCOPE_SINGLETON)
+
+@Component
+public class SingletonBean {
+
+    public SingletonBean() {
+        System.out.println("Creating a Singleton bean!!!");
+    }
+
+    public String getMyScope(){
+        return "I'm a singleton";
+    }
+}
